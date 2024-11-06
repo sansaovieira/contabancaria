559b96a04874ad4d52a7aa20803a5a31320bd4bc
sansaovieira <vieirasansao42@gmail.com>
2024-11-06 13:08:04 -0300

    feat(CriaçãoMenu): Criação das classe menu e cores

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -3,7 +3,18 @@ package conta;
 public class Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("\n********************* Banco Vieira💲, LDA ***************************");
+		System.out.println("\n                      ____OPERAÇÕES____");
+		System.out.println("\n                        📝 1 - Saldo");
+		System.out.println("                        📝 2 - Fazer Depósito");
+		System.out.println("                        📝 3 - Saque");
+		System.out.println("                        📝 4 - Pagamentos");
+		System.out.println("                        📝 5 - Transferências");
+		System.out.println("                        📝 6 - Extrato");
+		System.out.println("                        📝 7 - Sair");
+		System.out.println("\n                        📌 Escolha uma opção 📌");
+		System.out.println("\n**************************************************************************");
 
 	}
 
--- src/conta/util/Cores.java
+++ src/conta/util/Cores.java
@@ -2,9 +2,11 @@ package conta.util;
 
 public class Cores {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	public static final String DEFAULT = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String CYAN = "\u001B[36m";
 
 }
