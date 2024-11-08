3f408ed15183a1792836a54eb1c409bd84a9d1df
sansaovieira <vieirasansao42@gmail.com>
2024-11-08 13:37:20 -0300

    feat(ReajustarDadosConta): add user interaction

--- src/conta/model/Menu.java
+++ src/conta/model/Menu.java
@@ -5,6 +5,7 @@ import conta.util.Cores;
 
 public class Menu {
 	public static void main(String[] args) {
+
 		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
 		c1.visualizar();
 		c1.sacar(12000.0f);
@@ -12,38 +13,38 @@ public class Menu {
 		c1.depositar(5000.0f);
 		c1.visualizar();
 
-		Scanner leia = new Scanner(System.in);
-
-		int opcao;
-
 		while (true) {
 
-			System.out.println(Cores.YELLOW + Cores.DEFAULT
-					+ "*****************************************************");
-			System.out.println("                                                     ");
-			System.out.println("                BANCO DO BRAZIL COM Z                ");
-			System.out.println("                                                     ");
-			System.out.println("*****************************************************");
-			System.out.println("                                                     ");
-			System.out.println("            1 - Criar Conta                          ");
-			System.out.println("            2 - Listar todas as Contas               ");
-			System.out.println("            3 - Buscar Conta por Numero              ");
-			System.out.println("            4 - Atualizar Dados da Conta             ");
-			System.out.println("            5 - Apagar Conta                         ");
-			System.out.println("            6 - Sacar                                ");
-			System.out.println("            7 - Depositar                            ");
-			System.out.println("            8 - Transferir valores entre Contas      ");
-			System.out.println("            9 - Sair                                 ");
-			System.out.println("                                                     ");
-			System.out.println("*****************************************************");
-			System.out.println("Entre com a opção desejada:                          ");
+			System.out.println(
+					Cores.YELLOW + Cores.DEFAULT + "*****************************************************************");
+			System.out.println("                                                        ");
+			System.out.println("\n********************* Banco Vieira💲, LDA **************");
+			System.out.println("                                                        ");
+			System.out.println("********************************************************");
+			System.out.println("                                                        ");
+			System.out.println("            📝 1 - Criar Conta                          ");
+			System.out.println("            📝 2 - Listar todas as Contas               ");
+			System.out.println("            📝 3 - Buscar Conta por Numero              ");
+			System.out.println("            📝 4 - Atualizar Dados da Conta             ");
+			System.out.println("            📝 5 - Apagar Conta                         ");
+			System.out.println("            📝 6 - Sacar                                ");
+			System.out.println("            📝 7 - Depositar                            ");
+			System.out.println("            📝 8 - Transferir valores entre Contas      ");
+			System.out.println("            📝 9 - Sair                                 ");
+			System.out.println("                                                        ");
+			System.out.println("********************************************************");
+			System.out.println("Entre com a opção desejada:                             ");
 			System.out.println("                                                     " + Cores.DEFAULT);
+
+			Scanner leia = new Scanner(System.in);
+
+			int opcao;
 
 			opcao = leia.nextInt();
 
 			if (opcao == 9) {
 				System.out.println(Cores.GREEN + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
-                  		sobre();
+				sobre();
 				leia.close();
 				System.exit(0);
 			}
@@ -88,7 +89,7 @@ public class Menu {
 		}
 
 	}
-    
+
 	public static void sobre() {
 		System.out.println("\n*********************************************************");
 		System.out.println("Projeto Desenvolvido por: ");
