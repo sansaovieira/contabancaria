037791728dad51e70f6880a76921f41a03769819
sansaovieira <vieirasansao42@gmail.com>
2024-11-08 13:43:53 -0300

    feat(ResetAccountData): add user interaction, commit updated

--- src/conta/model/Menu.java
+++ src/conta/model/Menu.java
@@ -90,11 +90,12 @@ public class Menu {
 
 	}
 
-	public static void sobre() {
+	/* public static void sobre() {
 		System.out.println("\n*********************************************************");
 		System.out.println("Projeto Desenvolvido por: ");
 		System.out.println("Generation Brasil - generation@generation.org");
 		System.out.println("github.com/conteudoGeneration");
 		System.out.println("*********************************************************");
+	*/
 	}
 }
