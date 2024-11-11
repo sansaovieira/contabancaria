f543df7703796b0b6dd5f565cb55edbcc313d29f
sansaovieira <vieirasansao42@gmail.com>
2024-11-11 03:47:35 -0300

    feat(AddNewClasses): Add checking and savings account classes

--- src/conta/model/Menu.java
+++ src/conta/model/Menu.java
@@ -6,12 +6,29 @@ import conta.util.Cores;
 public class Menu {
 	public static void main(String[] args) {
 
-		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+		// Teste da Classe Conta
+		Conta c1 = new Conta(3, 123, 1, "Mariana", 500000.0f);
 		c1.visualizar();
 		c1.sacar(12000.0f);
 		c1.visualizar();
 		c1.depositar(5000.0f);
 		c1.visualizar();
+
+		// Teste da Classe Conta Corrente
+		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+
+		// Teste da Classe Conta Poupança
+		ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
 
 		while (true) {
 
@@ -90,12 +107,12 @@ public class Menu {
 
 	}
 
-	/* public static void sobre() {
+	public static void sobre() {
 		System.out.println("\n*********************************************************");
 		System.out.println("Projeto Desenvolvido por: ");
 		System.out.println("Generation Brasil - generation@generation.org");
 		System.out.println("github.com/conteudoGeneration");
 		System.out.println("*********************************************************");
-	*/
+
 	}
 }
